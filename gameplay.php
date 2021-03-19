4ee012fcc8f42718b0ea35048c7ccb2cd26fccf9
Thomadeus <emailtommy04@gmail.com>
2021-03-18 21:48:51 -0400

    initial commit

---
+++ gameplay.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="game.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="tool_bar">
+    </div>
+
+    <div id="game_board">
+        
+        <div class="flex_item red"><a href="g5subjects.php">5th Grade Subject </a></div>
+        <div class="flex_item red"> 5th Grade Subject</div>
+        <div class="flex_item yellow"> 4th Grade Subject</div>
+        <div class="flex_item yellow"> 4th Grade Subject</div>
+        <div class="flex_item blue">3rd Grade Subject</div>
+        <div class="flex_item blue">3rd Grade Subject</div>
+        <div class="flex_item purple">2nd Grade Subject</div>
+        <div class="flex_item purple">2nd Grade Subject</div>
+        <div class="flex_item green">1st Grade Subject</div>
+        <div class="flex_item green">1st Grade Subject</div>
+    </div>
+</body>
+</html>
